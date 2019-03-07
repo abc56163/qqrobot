c6e9cb7710922ad125a3ee95fb00a5bd27dbd3a6
root <root@ecs-5232.novalocal>
2019-03-07 15:20:55 +0800

    sql search

--- plugins/reply.py
+++ plugins/reply.py
@@ -4,7 +4,7 @@ from nonebot import on_natural_language, NLPSession, IntentCommand
 import os
 import time
 import jieba
-jieba.load_userdict('dict.txt')
+jieba.load_userdict(os.path.dirname(__file__)+'/dict.txt')
 import MySQLdb
 db = MySQLdb.connect("localhost", "root", "Abcd520025@", "study", charset='utf8')
 cursor = db.cursor()
@@ -18,7 +18,10 @@ async def reply(session: CommandSession):
     msg_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     with open(os.path.dirname(__file__)+'/'+'QQ_msg_log.txt', 'a') as qq:
         qq.write('{}{}{}'.format(msg_time + '\n', 'From:' + message, '\n' + 'To:' + reply + '\n'))
-    await session.send(reply)
+    if reply == EXPR_DONT_UNDERSTAND:
+        pass
+    else:
+        await session.send(reply)
 
 
 @on_natural_language(only_to_me=False)
@@ -28,21 +31,25 @@ async def _(session: NLPSession):
 
 async def database_search(session: CommandSession, msg: str)-> Optional[str]:
     try:
-        cursor.execute('select * from s1 where code LIKE "%s"' % msg)
+        cursor.execute('select * from robot where keyword LIKE "%s"' % msg)
         a = cursor.fetchall()
-        t = True
-        if a == ():
+        text = a[0][2]
+        return text
+    except:
+        try:
             seg_list = jieba.cut(msg, cut_all=True)
+            t = True
             while t:
-                cursor.execute('select * from s1 where code LIKE "%{}%"' .format(next(seg_list)))
+                cursor.execute('select * from robot where keyword LIKE "%{}%"'.format(next(seg_list)))
                 a = cursor.fetchall()
-                text = a[0][1]
                 if a != ():
+                    text = a[0][2]
                     t = False
             return text
-    except:
-        return EXPR_DONT_UNDERSTAND
-        # return 'this is test'
+        except:
+            text = EXPR_DONT_UNDERSTAND
+            return text
+        # return None
 
 
 
