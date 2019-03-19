43c30f183c69c2343f8b59e9f68ecc13276da1a6
root <root@ecs-5232.novalocal>
2019-03-19 09:45:01 +0800

    新增关键字搜索，数据库添加了表2

--- plugins/reply.py
+++ plugins/reply.py
@@ -3,28 +3,37 @@ from nonebot import on_command, CommandSession, on_notice
 from nonebot import on_natural_language, NLPSession, IntentCommand
 import os
 import time
+import MySQLdb
 import jieba
 jieba.load_userdict(os.path.dirname(__file__)+'/dict.txt')
-import MySQLdb
 
 
-db = MySQLdb.connect("localhost", "root", "Abcd520025@", "study", charset='utf8')
+db = MySQLdb.connect("localhost", "root", "Abcd520025@", "58dh", charset='utf8')
 cursor = db.cursor()
 
 EXPR_DONT_UNDERSTAND = '未匹配到关键词'
 
+
 @on_command('reply')
 async def reply(session: CommandSession):
-    message = session.state.get('message').replace(' ','')
-    reply = await database_search(session, message)
+    message = session.state.get('message').replace(' ', '')
+    # print(message,1)
+    if message in ('h电话', 'h电脑', 'h网络'):
+        table = '58_robot_1'
+        reply = await database_search(session, table, message)
+        des = ''
+    else:
+        table = '58_robot_2'
+        reply = await database_search(session, table, message)
+        des = "\n\n更对解决方案可以输入下列关键词：h电话,h电脑,h网络来获取更多帮助\n您也可以直接美式扫工位二维码或online在线提单来快速联系IT"
     msg_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     with open(os.path.dirname(__file__)+'/'+'QQ_msg_log.txt', 'a') as qq:
         qq.write('{}{}{}'.format(msg_time + '\n', 'From:' + message, '\n' + 'To:' + reply + '\n'))
     if reply == EXPR_DONT_UNDERSTAND:
         pass
     else:
-        await session.send(reply+"\n\n更对解决方案可以输入下列关键词：电话问题 电脑问题 网络问题，来获取更多帮助\n您也可以直接"
-                                 "美式扫工位二维码或online在线提单来快速联系IT")
+        await session.send(reply+'\n'+des)
+
 
 
 @on_natural_language(only_to_me=False)
@@ -32,20 +41,20 @@ async def _(session: NLPSession):
     return IntentCommand(60.0, 'reply', args={'message': session.msg_text})
 
 
-async def database_search(session: CommandSession, msg: str)-> Optional[str]:
-    cursor.execute('select * from robot where keyword LIKE "%{}%";'.format(msg) )
+async def database_search(session: CommandSession, table,msg: str)-> Optional[str]:
+    cursor.execute('select * from {} where keyword LIKE "{}";'.format(table, msg))
     a = cursor.fetchall()
-    #print(a)
+    # print(a)
     if a != ():
         text = a[0][2]
         return text
     else:
         try:
             seg_list = jieba.cut(msg, cut_all=False)
-            #print(jieba.lcut(msg, cut_all=False))
+            # print(jieba.lcut(msg, cut_all=False))
             t = True
             while t:
-                cursor.execute('select * from robot where keyword LIKE "{}";'.format(next(seg_list)))
+                cursor.execute('select * from {} where keyword LIKE "%{}%";'.format(table, next(seg_list)))
                 a = cursor.fetchall()
                 if a != ():
                     text = a[0][2]
