79e6267e93507d929472014c151a35cc8cb0c821
root <root@ecs-5232.novalocal>
2019-04-25 14:12:25 +0800

    修改数据库搜索

--- plugins/reply.py
+++ plugins/reply.py
@@ -36,10 +36,10 @@ async def reply(session: CommandSession):
         table = '58_robot_2'
         answer = await database_search(session, table, message)
         des = supplement
-    # if answer == EXPR_DONT_UNDERSTAND:
-    #     pass
-    # else:
-    #     await session.send(answer+'\n'+des)
+    if answer == EXPR_DONT_UNDERSTAND:
+        pass
+    else:
+        await session.send(answer+'\n'+des)
     msg_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     #聊天记录写入数据库
     cursor = mysql_connection(db).cursor()
@@ -55,8 +55,6 @@ async def reply(session: CommandSession):
     #     qq.write('{}{}{}'.format(msg_time + '\n', 'From:' + message, '\n' + 'To:' + reply + '\n'))
 
 
-
-
 @on_natural_language(only_to_me=False)
 async def _(session: NLPSession):
     return IntentCommand(60.0, 'reply', args={'message': session.msg_text})
@@ -64,28 +62,22 @@ async def _(session: NLPSession):
 
 async def database_search(session: CommandSession, table,msg: str)-> Optional[str]:
     cursor = mysql_connection(db).cursor()
-    cursor.execute('select * from {} where keyword LIKE "{}";'.format(table, msg))
-    a = cursor.fetchall()
-    # print(a)
-    if a != ():
-        text = a[0][2]
+    try:
+        seg_list = jieba.cut(msg, cut_all=False)
+        print(jieba.lcut(msg, cut_all=False))
+        t = True
+        while t:
+            cursor.execute('select * from {} where keyword LIKE "{}";'.format(table, next(seg_list)))
+            a = cursor.fetchone()
+            print(a)
+            if a is not None:
+                text = a[2]
+                t = False
         return text
-    else:
-        try:
-            seg_list = jieba.cut(msg, cut_all=False)
-            # print(jieba.lcut(msg, cut_all=False))
-            t = True
-            while t:
-                cursor.execute('select * from {} where keyword LIKE "%{}%";'.format(table, next(seg_list)))
-                a = cursor.fetchall()
-                if a != ():
-                    text = a[0][2]
-                    t = False
-            return text
-        except StopIteration:
-            text = EXPR_DONT_UNDERSTAND
-            return text
-        # return None
+    except StopIteration:
+        text = EXPR_DONT_UNDERSTAND
+        return text
+    # return None
 
 
 
