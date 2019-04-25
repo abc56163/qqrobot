2aec14dc3db86b36e541379b91952e8a9d9bcb54
root <root@ecs-5232.novalocal>
2019-04-25 08:29:53 +0800

    问答日至写入数据库

--- plugins/reply.py
+++ plugins/reply.py
@@ -7,11 +7,12 @@ import MySQLdb
 import jieba
 jieba.load_userdict(os.path.dirname(__file__)+'/dict.txt')
 
-db = MySQLdb.connect("localhost", "root", "Abcd520025@", "58dh", charset='utf8')
 
 # 保持mysql长连接
-def mysql_connection():
-    global db
+db = MySQLdb.connect("localhost", "root", "Abcd520025@", "58dh", charset='utf8')
+
+
+def mysql_connection(db):
     try:
         db.ping()
     except:
@@ -19,7 +20,7 @@ def mysql_connection():
     return db
 
 
-cursor = mysql_connection().cursor()
+
 
 EXPR_DONT_UNDERSTAND = '未匹配到关键词'
 
@@ -35,13 +36,25 @@ async def reply(session: CommandSession):
         table = '58_robot_2'
         reply = await database_search(session, table, message)
         des = "\n\n更对解决方案可以输入下列关键词：h电话,h电脑,h网络来获取更多帮助\n您也可以直接美式扫工位二维码或online在线提单来快速联系IT"
-    msg_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    with open(os.path.dirname(__file__)+'/'+'QQ_msg_log.txt', 'a') as qq:
-        qq.write('{}{}{}'.format(msg_time + '\n', 'From:' + message, '\n' + 'To:' + reply + '\n'))
     # if reply == EXPR_DONT_UNDERSTAND:
     #     pass
     # else:
     #     await session.send(reply+'\n'+des)
+    msg_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    #聊天记录写入数据库
+    cursor = mysql_connection(db).cursor()
+    try:
+        cursor.execute('insert into 58_robot_3 (im, time, question, answer) values ("QQ", "{}", "{}", "{}")'
+                   .format(msg_time, message, reply))
+        db.commit()
+    except:
+        db.rollback()
+
+    # 聊天记录写入文件
+    # with open(os.path.dirname(__file__)+'/'+'QQ_msg_log.txt', 'a') as qq:
+    #     qq.write('{}{}{}'.format(msg_time + '\n', 'From:' + message, '\n' + 'To:' + reply + '\n'))
+
+
 
 
 
@@ -51,6 +64,7 @@ async def _(session: NLPSession):
 
 
 async def database_search(session: CommandSession, table,msg: str)-> Optional[str]:
+    cursor = mysql_connection(db).cursor()
     cursor.execute('select * from {} where keyword LIKE "{}";'.format(table, msg))
     a = cursor.fetchall()
     # print(a)
